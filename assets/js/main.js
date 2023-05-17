6b94267d971538d72b6c212cd91d2f599d19335d
Daniel <63712309+Daniel-Hutanu@users.noreply.github.com>
2023-05-17 18:23:57 +0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+// const canvas = document.getElementById("canvas");
+// const ctx = canvas.getContext("2d");
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
+
+// let radiusCircle = -2;
+//   radiusCircle += 25;
+//   const radius = radiusCircle;
+
+//   ctx.strokeStyle = "#fff";
+//   ctx.lineWidth = 1;
+//   const x = canvas.width / 2;
+//   const y = canvas.height / 2;
+
+//   ctx.beginPath();
+//   ctx.arc(x, y, radius, 0, 2 * Math.PI);
+//   ctx.stroke();
+
